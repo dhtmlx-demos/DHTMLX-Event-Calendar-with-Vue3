a89b9fd5589a7cd58112f749c4ee62829ec6c45f
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:09:12 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Calendar from "./components/Calendar.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Calendar },
+  data() {
+    return {
+      records: getData(),
+      date: new Date(2021, 5, 10),
+    };
+  },
+};
+</script>
+
+<template>
+  <Calendar :events="records" :date="date" />
+</template>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,22 @@
+<script>
+import { EventCalendar } from "@dhx/trial-eventcalendar";
+import "@dhx/trial-eventcalendar/dist/event-calendar.css";
+
+export default {
+  props: ["events", "date"],
+
+  mounted() {
+    new EventCalendar(this.$refs.cont, {
+      events: this.events,
+      date: this.date,
+    });
+  },
+  unmounted() {
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div ref="cont" style="width: 100%; height: 100%"></div>
+</template>
